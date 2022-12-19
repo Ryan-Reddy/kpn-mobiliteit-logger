5c2e1e714320b5ff2d11cd30fa6913ed5e955479
Ryan Reddy <90551370+klugook@users.noreply.github.com>
2022-12-19 14:21:45 +0100

    add print button -working add export to CSV -notworking

--- vite-project/overzicht-reizen.js
+++ vite-project/overzicht-reizen.js
@@ -100,6 +100,51 @@ export class MyElement extends LitElement {
     // }
   }
 
+  //TODO: make tableToCSV work with lit
+  tableToCSV() {
+  //   console.log('tableToCSV');
+  //   // Variable to store the final csv data
+  //   var csv_data = [];
+  //   // Get each row data
+  //   var rows = document.getElementsByTagName('tr');
+  //   for (var i = 0; i < rows.length; i++) {
+  //     // Get each column data
+  //     var cols = rows[i].querySelectorAll('td,th');
+  //     // Stores each csv row data
+  //     var csvrow = [];
+  //     for (var j = 0; j < cols.length; j++) {
+  //       // Get the text data of each cell of a row and push it to csvrow
+  //       csvrow.push(cols[j].innerHTML);
+  //     }
+  //     csv_data.push(csvrow.join(","));
+  //   }
+  //   csv_data = csv_data.join('\n');
+  // }
+  //
+  // downloadCSVFile(csv_data) {
+  //
+  //   // Create CSV file object and feed our
+  //   // csv_data into it
+  //   let CSVFile = new Blob([csv_data], { type: "text/csv" });
+  //
+  //   // Create to temporary link to initiate
+  //   // download process
+  //   var temp_link = document.createElement('a');
+  //
+  //   // Download csv file
+  //   temp_link.download = "GfG.csv";
+  //   var url = window.URL.createObjectURL(CSVFile);
+  //   temp_link.href = url;
+  //
+  //   // This link should not be displayed
+  //   temp_link.style.display = "none";
+  //   document.body.appendChild(temp_link);
+  //
+  //   // Automatically click the link to trigger download
+  //   temp_link.click();
+  //   document.body.removeChild(temp_link);
+  }
+
   render() {
     return html`
         <br>
@@ -146,7 +191,8 @@ export class MyElement extends LitElement {
         </div>
         </span>
             <button>Exporteren als..</button>
-            <button>Print...</button>
+            <button @click="${this.tableToCSV}">download CSV</button>
+            <button onclick="print()">Print...</button>
         </main>
 
 
