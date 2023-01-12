8d0b1fe842746bd6d064f2d186460daec848f096
Ryan Reddy <90551370+klugook@users.noreply.github.com>
2023-01-12 05:21:12 +0100

    add web-dev

---
+++ web-dev-server.config.js
@@ -0,0 +1,19 @@
+export default {
+
+  open: true,
+
+  watch: true,
+
+  appIndex: 'index.html',
+
+  nodeResolve: {
+
+    exportConditions: ['development'],
+
+    dedupe: true,
+
+  },
+
+  esbuildTarget: 'auto',
+
+};
