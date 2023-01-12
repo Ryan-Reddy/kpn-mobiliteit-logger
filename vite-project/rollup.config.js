0c355cd4d2a1fe271b843f0187b85555a251d37f
Ryan Reddy <90551370+klugook@users.noreply.github.com>
2023-01-12 01:27:36 +0100

    changed adress in rollup.config.js

--- vite-project/rollup.config.js
+++ vite-project/rollup.config.js
@@ -16,7 +16,7 @@ const copyConfig = {
 // The main JavaScript bundle for modern browsers that support
 // JavaScript modules and other ES2015+ features.
 const config = {
-  input: './src/controller-template.ts',
+  input: 'src/controller-template.ts',
   output: {
     dir: 'build-modern/src/components',
     format: 'es',
