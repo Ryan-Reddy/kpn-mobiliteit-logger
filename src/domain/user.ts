26b33b1a9a1f77ba964a4c9ad0fe2de85d2a1c95
Ryan Reddy <90551370+klugook@users.noreply.github.com>
2023-01-27 23:16:11 +0100

    add current wiki incl ontwerpdocu to /wiki/

--- src/domain/user.ts
+++ src/domain/user.ts
@@ -1,13 +1,17 @@
+import {reisDTO} from "./reisDTO";
+
 export default class User {
     private _id: number;
     private naam: string;
     private _team: string;
     private _reizen: any[];
+    private _projecten: any[];
 
-    constructor(id: number, naam: string, team: string, reizen: any[]) {
+    constructor(id: number, naam: string, team: string, reizen: reisDTO[], projecten: string[]) {
         this._id = id;
         this.naam = naam;
         this._team = team;
+        this._projecten = projecten;
         this._reizen = reizen;
         // TODO setup DTO for service layer
     }
